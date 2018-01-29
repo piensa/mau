055836c0fe3833f1710c1d5b5f72c2e5e3867084
Lennin <lenninlasd@gmail.com>
2018-01-29 20:53:09 +0000

    View test and coverage comments on slack bot

--- cmd/cmd_list.go
+++ cmd/cmd_list.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"encoding/json"
 	"fmt"
+	"strings"
 )
 
 func init() {
@@ -23,13 +24,14 @@ func init() {
 		"List open pull requests.",
 		func(conv hanu.ConversationInterface) {
 			pr := getPrList(GithubToken)
-			fmt.Println(pr)
+
 			answer := "No new features to list"
 
 			if len(pr) > 0 {
 				answer = "Open Pull Requests:\n"
 				for i := 0; i < len(pr); i++ {
-					answer = answer + "`" + pr[i].Head.Ref + "` " + pr[i].Html_url + " \n"
+					commentBody := getCommentBody(GithubToken, pr[i].Links.Comments.Href)
+					answer = answer + "`" + pr[i].Head.Ref + "` " + pr[i].Html_url + " \n" + commentBody
 				}
 			}
 			conv.Reply(answer)
@@ -38,25 +40,45 @@ func init() {
 }
 
 type Prlist struct {
-	Url string `json:"url"`
-	Html_url string `json:"html_url"`
-	State string `json:"state"`
-	Head Prhead `json:"head"`
+	Url		string	`json:"url"`
+	Html_url	string	`json:"html_url"`
+	State		string	`json:"state"`
+	Links		PrLinks	`json:"_links"`
+	Head		Prhead	`json:"head"`
 }
 
 type Prhead struct {
-	Label string `json:"label"`
-	Ref string `json:"ref"`
-	Sha string `json:"sha"`
+	Label		string	`json:"label"`
+	Ref		string	`json:"ref"`
+	Sha		string	`json:"sha"`
 }
 
 type ConfigStruct struct {
-	GithubToken string `json:"GITHUB_ACCESS_TOKEN"`
+	GithubToken	string	`json:"GITHUB_ACCESS_TOKEN"`
+}
+
+type PrLinks struct {
+	Comments	HrefStr	`json:"comments"`
+}
+
+type HrefStr struct {
+	Href		string	`json:"href"`
+}
+
+type Comment struct {
+	Body		string	`json:"body"`
+}
+
+type ParsedComment struct {
+	DeploymentUrl	string
+	DeploymentDate	string
+	TestCoverage	string
+	testPassed	string
+	TestTime	string
 }
 
 func getPrList(GithubToken string) []Prlist {
 	url := "https://api.github.com/repos/geosure/geosure/pulls?access_token=" + GithubToken
-
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
@@ -69,4 +91,35 @@ func getPrList(GithubToken string) []Prlist {
 	json.Unmarshal(body, &pr)
 
 	return pr
+}
+
+func getCommentBody(GithubToken string, commentUrl string) string {
+	url :=  commentUrl +  "?access_token=" + GithubToken
+
+	resp, err :=  http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	var comment []Comment
+	json.Unmarshal(body, &comment)
+
+	commentBody := ""
+
+	for i := 0; i < len(comment); i++ {
+		if strings.Contains(comment[i].Body, "deployment:url") {
+			commentBody = comment[i].Body
+		}
+	}
+	stringCommentParser(commentBody)
+	return commentBody
+
+}
+
+func stringCommentParser(commentBody string) {
+	commentArray := strings.Split(commentBody, "\n")
+	fmt.Println(commentArray[0])
 }
