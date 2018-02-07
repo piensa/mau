89449b1795445eb12ad81f3e57bcb57ca7d16706
Way Barrios <waybarrios@gmail.com>
2018-02-07 15:36:50 -0500

    prometheus configuration command

--- cmd/cmd_prometheus.go
+++ cmd/cmd_prometheus.go
@@ -8,38 +8,75 @@ import (
 	"fmt"
 	"log"
 	"strings"
+	"regexp"
+	"os"
+	"io"
+	"os/exec"
 )
 
 
 func init() {
-	config_Prometheus ="/Users/waybarrios/Documents/code/prometheus"
+	config_Prometheus := "/Users/waybarrios/Documents/code/prometheus/"
 	Register(
-		"prometheus <endpoint:string>",
+		"prometheus <hash:string>",
 		"Set up config file in Prometheus  to ge the metrics",
 		func(conv hanu.ConversationInterface) {
-			endpoint, _ := conv.Match(0)
-			conv.Reply("Endpoint is `%s`", endpoint)
-			conv.Reply("Prometheus config path is `%s`",config_Prometheus)
-			parsing_yml(endpoint, config_Prometheus)
+			hash, _ := conv.Match(0)
+			conv.Reply("Hash  is `%s`", hash)
+			msg:=SetupPrometheus(hash, config_Prometheus)
+			conv.Reply("%s",msg)
 		},
 	)
 }
 
-func parsing_yml (endpoint string, config_path string) {
-	s := []string{config_path,"/","config.yml"};
+func ReloadPrometheus() {
+	pid, err := exec.Command("pgrep","prometheus").Output()
+    if err != nil {
+        log.Fatal(err)
+    }
+    arg := strings.Replace(string(pid), "\n", "", -1)
+    _ ,err = exec.Command("kill", "-HUP",arg).Output()
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    _, err = exec.Command("curl","-X","POST","http://nunez.co:9090/-/reload").Output()
+    if err != nil {
+        log.Fatal(err)
+    }
+}
+
+func SetupPrometheus (hash string, config_path string) string {
+
+	var IsLetter = regexp.MustCompile(`^[a-zA-Z]+$`).MatchString
+	if IsLetter(hash) == false {
+		return "Hash must only contain letters"
+	}
+	if len(hash) != 10 {
+		return "Invalid hash lenght. It must be lenght 10."
+	}
+	slice := []string{hash,".api.geosure.tech"}
+	endpoint :=strings.Join(slice, "")
+	
+	s := []string{config_path,"config.yml"};
 	yamlFile, err := ioutil.ReadFile(strings.Join(s, ""))
     if err != nil {
-        log.Printf("yamlFile.Get err   #%v ", err)
+        log.Fatal("yamlFile.Get err   #%v ", err)
+        
     }
     string_yaml := string(yamlFile)
     j2, err := yaml.YAMLToJSON([]byte(string_yaml))
+    fmt.Println(string(j2))
     if err != nil {
-    	log.Printf("Yaml parsing error...")
+    	log.Fatal("Yaml parsing error...")
+    	
     }
     var objmap map[string]interface{} 
 	err = json.Unmarshal(j2, &objmap)
 	if err != nil {
-		log.Printf("Json parsing error...")
+		log.Fatal("Json parsing error...")
+
 	}
 	
 	scrape_configs := objmap["scrape_configs"].([]interface{})
@@ -47,10 +84,63 @@ func parsing_yml (endpoint string, config_path string) {
 	static_conf := scrape["static_configs"].([]interface{})
 	static := static_conf[0].(map[string]interface{})
 	targets := static["targets"].([]interface{})
-
-	for _, value := range targets {
-		fmt.Println(value)
+	is_value := isValueInList(endpoint,targets)
+	if is_value == true {
+		return fmt.Sprintf("The endpoint `%s` has already been configured in Prometheus. Not need to update it.", endpoint)
 	}
-	
-    	
+
+	// adding new endpoint
+	targets = append(targets,endpoint)
+	static["targets"] = targets
+	var static_to_inter []interface {}
+	static_to_inter = append(static_to_inter,static)
+	scrape["static_configs"] = static_to_inter
+	var scrape_to_inter []interface {}
+	scrape_to_inter = append(scrape_to_inter,scrape)
+	objmap["scrape_configs"] = scrape_to_inter 
+	save:= saveYML(config_path,objmap)
+	if save == true {
+		ReloadPrometheus()
+		fmt.Sprintf("The endpoint `%s` has updated. :smile:", endpoint)
+	}
+
+}
+
+func saveYML (folder_path string, objmap map[string]interface{}) bool {
+	json, err := json.Marshal(objmap)
+	if err != nil { 
+		fmt.Println("err: %v\n", err)
+		return false
+		}
+	fmt.Println(folder_path)
+	yml, err := yaml.JSONToYAML(json)
+	if err != nil { 
+		fmt.Println("err: %v\n", err)
+		return false
+		}
+	string_yml := strings.Replace(string(yml), "null", "", -1)
+	fmt.Println(string_yml)
+	filepath := []string{folder_path,"config2.yml"}
+	fo, err := os.Create(strings.Join(filepath, ""))
+	if err != nil { 
+		fmt.Println("err: %v\n", err) 
+		return false
+		}
+	_, err = io.Copy(fo, strings.NewReader(string_yml))
+	if err != nil {
+		fmt.Println("err: %v\n", err) 
+		return false 
+		}
+	return true
+
+
+}
+
+func isValueInList(value string, list []interface{}) bool {
+    for _, v := range list {
+        if v == value {
+            return true
+        }
+    }
+    return false
 }
